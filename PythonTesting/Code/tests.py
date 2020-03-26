4ea17dbc25690b3d5de1a1c2bac3ea66f6abff1e
Dave Does Demos <46533954+davedoesdemos@users.noreply.github.com>
2020-03-26 12:01:28 +0000

    Update tests.py

--- PythonTesting/Code/tests.py
+++ PythonTesting/Code/tests.py
@@ -4,12 +4,12 @@ from pytest import approx
 def test_method1():
     x=3
     y=2
-    assert x == approx(y, 1)
+    assert x == approx(y, abs=1)
 
 def test_method2():
     x=3
     y=6
-    assert x == approx(y, 1)
+    assert x == approx(y, abs=1)
 
 def test_method3():
     x=3
