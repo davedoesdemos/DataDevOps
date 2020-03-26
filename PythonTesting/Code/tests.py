414a6ab79585ce057dfdc5e6c8f19bca7908bb89
Dave Does Demos <46533954+davedoesdemos@users.noreply.github.com>
2020-03-26 09:47:55 +0000

    pytest

---
+++ PythonTesting/Code/tests.py
@@ -0,0 +1,16 @@
+import pytest
+
+def test_method1():
+    x=2
+    y=3
+    assert x+1 == y,"test failed"
+
+def test_method2():
+    x=3
+    y=6
+    assert x+1 == y,"test failed" 
+
+def test_method3():
+    x=2
+    y=3
+    assert x+0 == y,"test failed"
