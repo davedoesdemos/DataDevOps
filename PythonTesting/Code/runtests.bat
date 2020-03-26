c101cf456d48040ebcce1a57a798b92125c0c3f2
Dave Does Demos <46533954+davedoesdemos@users.noreply.github.com>
2020-03-26 11:50:40 +0000

    changed tests

--- PythonTesting/Code/runtests.bat
+++ PythonTesting/Code/runtests.bat
@@ -1,4 +1,2 @@
-cd
 pip install pytest
-cd
 pytest PythonTesting\Code\tests.py --junitxml=PythonTesting\Code\results.xml
--- PythonTesting/Code/tests.py
+++ PythonTesting/Code/tests.py
@@ -2,16 +2,16 @@ import pytest
 from pytest import approx
 
 def test_method1():
-    x=2
-    y=3
-    assert x+1 == approx(y, 1),"test failed"
+    x=3
+    y=2
+    assert x == approx(y, 1),"test failed"
 
 def test_method2():
     x=3
     y=6
-    assert x+1 == y,"test failed" 
+    assert x == y,approx(y, 1),"test failed" 
 
 def test_method3():
-    x=2
-    y=3
-    assert x+0 == y,"test failed"
+    x=3
+    y=2
+    assert x == y,"test failed"
